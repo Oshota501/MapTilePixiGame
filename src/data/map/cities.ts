ba7f8656ae4cccdb02c2ab3f94c61eb916bd305a
Oshota501 <kakidesu1111@gmail.com>
2025-11-09 23:35:30 +0900

    feat : getCity　メソット追加

--- src/data/map/cities.ts
+++ src/data/map/cities.ts
@@ -5,7 +5,22 @@ import { City } from "./city";
 export class CitiesDB extends Container {
     public major : Container = new Container() ;
     public satellite : Container = new Container() ;
+
     public city : City[] = [] ;
+    /**
+     * 名前から都市を検索
+     * @param cityname 
+     * @returns [city , isSuccess]
+     */
+    public getCity(cityname:string):[City,true]|[null,false]{
+        for(let i = 0 ; i < this.city.length ; i ++){
+            const elm = this.city[i] ;
+            if(elm.cityName == cityname){
+                return [elm,true] ;
+            }
+        }
+        return [null,false] ;
+    }
     constructor(){
         super();
         this.addChild(this.major) ;
